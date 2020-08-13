a92b7ea3b42a3e478eb76cf9aa112e567e554f80
Clinton Kral <46661683+ClintonKral@users.noreply.github.com>
2020-08-13 16:51:36 -0700

    Add files via upload
    Added loading screen and fixed navbar donation link

--- bay.js
+++ bay.js
@@ -15,8 +15,10 @@ const navrSlide = () => {
             }
             else{
                 link.style.animation = `navBarIn 0.5s ease forwards ${index / 7 + 0.5}s`;
+                document.getElementById('lol').style.top = "264px"
             }
         });
+
         //smush lines Animations
         smush.classList.toggle('turn');
     });
@@ -32,11 +34,19 @@ function navBarScroll()
     if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40)
     {
         document.getElementById('navbar').style.padding = "20px 50px";
+        document.getElementById('lol').style.top = "69px";
     }
     else
     {
         document.getElementById('navbar').style.padding= "40px 50px";
+        document.getElementById('lol').style.top = "89px";
     }
 }
+
+//hides loading screen when done
+window.addEventListener("load", function() {
+    const loader = document.querySelector(".loading");
+    loader.className += " hidden";
+});
 
 navrSlide();
